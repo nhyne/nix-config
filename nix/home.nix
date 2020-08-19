9331d7ec6bace0e11ba3f03ebc8f5e219c907f42
nhyne <nhyne@nhyne.dev>
2020-08-19 08:29:08 -0400

    installing some packages

---
+++ nix/home.nix
@@ -0,0 +1,46 @@
+# https://nixos.wiki/wiki/Home_Manager
+
+# Stuff on this file, and ./*.nix, should work across all of my computing
+# devices. Presently these are: Thinkpad, Macbook and Pixel Slate.
+
+{ config, pkgs, device ? "unknown", ...}:
+
+let
+  imports = [
+    ./git.nix
+    ./haskell.nix
+    ./shells.nix
+    ./tmux.nix
+    ./emacs
+  ];
+in
+{
+  nixpkgs.config.allowUnfree = true;
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # To track sources
+    niv
+
+    # Basic tools
+    htop
+    #file
+    jq
+    #youtube-dl
+
+
+    # Dev tools
+    #gnumake
+    #ripgrep
+    #tig
+    #tmate
+    #gitAndTools.gh
+    dhall
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+}
