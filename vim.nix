f37a7413515372b3ada0465a038a09f1cf1d9020
nhyne <nhyne@nhyne.dev>
2021-05-11 19:57:41 -0400

    cleanup

--- vim.nix
+++ vim.nix
@@ -1,16 +1,16 @@
 { pkgs, lib, ... }:
 
 {
-    programs.neovim = with pkgs.vimPlugins; {
-        enable = true;
-        vimAlias = true;
-        viAlias = true;
-        vimdiffAlias = true;
-        plugins = [
-            ale
-            vim-airline
-            zenburn
-        ];
-        extraConfig = lib.fileContents ./vimrc;
-    };
+  programs.neovim = with pkgs.vimPlugins; {
+    enable = true;
+    vimAlias = true;
+    viAlias = true;
+    vimdiffAlias = true;
+    plugins = [
+      ale
+      vim-airline
+      zenburn
+    ];
+    extraConfig = lib.fileContents ./vimrc;
+  };
 }
