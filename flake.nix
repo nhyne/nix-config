52ed58787d1c63137b7050490f35972d067f4fef
nhyne <nhyne@nhyne.dev>
2021-06-03 21:26:31 -0400

    config

--- flake.nix
+++ flake.nix
@@ -34,6 +34,7 @@
             home-manager.users.nhyne = import ./home.nix {
               inherit inputs system;
               pkgs = import nixpkgs { inherit system; };
+              inherit config;
             };
           }
         ] ++ extraModules);
