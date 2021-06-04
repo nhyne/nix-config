284ed5f98cc041b099457271c98e4a22ca22e918
nhyne <nhyne@nhyne.dev>
2021-06-03 21:34:32 -0400

    user

--- hosts/server1.nix
+++ hosts/server1.nix
@@ -11,6 +11,11 @@
     scala
   ];
 
+  users.users.nhyne = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
    # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
