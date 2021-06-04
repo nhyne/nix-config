9aa4ed91bc4888dc60280b39196fb8719c069f5a
nhyne <nhyne@nhyne.dev>
2021-06-03 21:21:22 -0400

    adding flake file

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "nhyne's NixOS configuration";
+
+  inputs = {
+    # To update nixpkgs (and thus NixOS), pick the nixos-unstable rev from
+    # https://status.nixos.org/
+    #
+    # This ensures that we always use the official # cache.
+    nixpkgs.url = "github:nixos/nixpkgs/1c2986bbb806c57f9470bf3231d8da7250ab9091";
+
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
+  outputs = inputs@{ self, home-manager, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      # Make configuration for any computer I use in my home office.
+      mkHomeMachine = configurationNix: extraModules: nixpkgs.lib.nixosSystem {
+        inherit system;
+        # Arguments to pass to all modules.
+        specialArgs = { inherit system inputs; };
+        modules = ([
+          # System configuration
+          configurationNix
+
+          # Features common to all of my machines
+          ./features/docker.nix
+          # home-manager configuration
+          home-manager.nixosModules.home-manager
+          {
+            #home-manager.useGlobalPkgs = true;
+            #home-manager.useUserPackages = true;
+            home-manager.users.nhyne = import ./home.nix {
+              inherit inputs system;
+              pkgs = import nixpkgs { inherit system; };
+            };
+          }
+        ] ++ extraModules);
+      };
+    in
+    {
+      nixosConfigurations.server1 = mkHomeMachine
+        ./hosts/server1.nix
+        [
+        #  inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p53
+        ];
+    };
+}
