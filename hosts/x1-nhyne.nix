3f51a8b048dec9fe6ad8774a45e3a5f7b2219660
nhyne <nhyne@nhyne.dev>
2021-06-15 22:15:13 -0400

    bazel and updated ssh key

--- hosts/x1-nhyne.nix
+++ hosts/x1-nhyne.nix
@@ -29,6 +29,10 @@
 
   networking.hostName = "x1-nhyne";
 
+  environment.systemPackages = with pkgs; [
+    bazel
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
