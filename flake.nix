383d183fea205f21553489826c2c7dd992775860
nhyne <nhyne@nhyne.dev>
2021-10-07 15:54:24 -0400

    downgrading to get good intellij version

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # https://status.nixos.org/
     #
     # This ensures that we always use the official # cache.
-    nixpkgs.url = "github:nixos/nixpkgs/e4f1de134350738207ca66162707a1306a5d1a1c";
+    nixpkgs.url = "github:nixos/nixpkgs/7067d12bb5154c85c712177fbca42907cfd06722";
     nixos-hardware.url = github:NixOS/nixos-hardware/342048461da7fc743e588ee744080c045613a226;
     home-manager.url = "github:nix-community/home-manager/039f786e609fdb3cfd9c5520ff3791750c3eaebf";
   };
